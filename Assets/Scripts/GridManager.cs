4d946af8b5723dc168d7f2b8fb8acd66c2a7ede7
jsyeom <jsyeom@allm.co.kr>
2023-02-02 11:06:01 +0900

    135. 딕셔너리

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -1,21 +1,26 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GridManager : MonoBehaviour
 {
-    [SerializeField] Node node;
+    [SerializeField] private Vector2Int gridSize;
+    private Dictionary<Vector2Int, Node> grid = new Dictionary<Vector2Int, Node>();
 
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        Debug.Log(node.coordinates);
-        Debug.Log(node.isWalkable);
+        CreateGrid();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void CreateGrid()
     {
-        
+        for (int x = 0; x < gridSize.x; x++)
+        {
+            for (int y = 0; y < gridSize.y; y++)
+            {
+                Vector2Int coordinate = new Vector2Int(x, y);
+                grid.Add(coordinate, new Node(coordinate, true));
+                Debug.Log(grid[coordinate].coordinates + "=" + grid[coordinate].isWalkable);
+            }
+        }
     }
 }
